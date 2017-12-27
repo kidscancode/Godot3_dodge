0bed7fc0da88ff4210eca18600b6f81951e84b3d
Chris Bradfield <chris@kidscancode.org>
2017-12-27 09:20:51 -0800

    restore random angle (removed during Path2D bug)

--- Main.gd
+++ Main.gd
@@ -29,7 +29,7 @@ func _on_MobTimer_timeout():
 	var direction = $MobPath/MobSpawnLocation.rotation + PI/2
 	mob.position = $MobPath/MobSpawnLocation.position
 	# add some randomness to the direction
-	#direction += rand_range(-PI/4, PI/4)
+	direction += rand_range(-PI/4, PI/4)
 	mob.rotation = direction
 	mob.set_linear_velocity(Vector2(rand_range(mob.MIN_SPEED, mob.MAX_SPEED), 0).rotated(direction))
 	
