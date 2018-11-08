b2e580b1f123117f09529cc061274f15c7baf9f1
Cody Graves <cody.graves@hotmail.com>
2018-11-08 12:02:59 -0800

    Fixed player controller

--- Player.gd
+++ Player.gd
@@ -18,26 +18,26 @@ func start(pos):
 
 func _process(delta):
 	velocity = Vector2()
-	#if (is_network_master()):
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * SPEED
-		$AnimatedSprite.play()
-		$Trail.emitting = true
-	else:
-		$AnimatedSprite.stop()
-		$Trail.emitting = false
+	if (is_network_master()):
+		if Input.is_action_pressed("ui_right"):
+			velocity.x += 1
+		if Input.is_action_pressed("ui_left"):
+			velocity.x -= 1
+		if Input.is_action_pressed("ui_down"):
+			velocity.y += 1
+		if Input.is_action_pressed("ui_up"):
+			velocity.y -= 1
+		if velocity.length() > 0:
+			velocity = velocity.normalized() * SPEED
+			$AnimatedSprite.play()
+			$Trail.emitting = true
+		else:
+			$AnimatedSprite.stop()
+			$Trail.emitting = false
 
-		position += velocity * delta
-		position.x = clamp(position.x, 0, screensize.x)
-		position.y = clamp(position.y, 0, screensize.y)
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
 
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "right"
